16f16f550eac2ca1d13ef137e8a9c92faebee0ab
Amin Rayej <m.amin.rayej@gmail.com>
2019-04-24 15:58:53 +0430

    deleted peecky project in shamekhi branch.

--- peecky/peecky_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class PeeckyAppConfig(AppConfig):
-    name = 'peecky_app'
--- peecky/peecky_app/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class contact_number(models.Model):
-    number = models.CharField(max_length=16)
